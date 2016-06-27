e696312e2f51641ffed744c9fd2cfb5d34741438
Devin <devinwiebelt@gmail.com>
2016-06-27 11:42:27 -0400

    added more start data to friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,21 +1,24 @@
-var friends =
+var friends = [
 	{	
   "name":"King Bob",
   "photo":"https://s-media-cache-ak0.pinimg.com/564x/e8/3e/fc/e83efc660ae1fd1d420dacb58d00d953.jpg",
-  "scores":[
-     5,
-     1,
-     4,
-     4,
-     5,
-     1,
-     2,
-     5,
-     4,
-     1
-   ]
-}
+  "scores":[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+  },
 
+  {
+  "name":"Stitch",
+  "photo":"http://vignette3.wikia.nocookie.net/stitchdatabase/images/f/f2/Stitch_(Lilo_%26_Stitch).png/revision/20151206013305",
+  "scores":[3, 2, 3, 4, 1, 5, 3, 4, 2, 3]
+
+  },
+
+  {
+  "name":"Donald Duck",
+  "photo":"https://upload.wikimedia.org/wikipedia/en/thumb/b/b4/Donald_Duck.svg/618px-Donald_Duck.svg.png",
+  "scores":[1, 4, 5, 2, 5, 3, 2, 4, 4, 5]
+  },
+
+];
 
 
 module.exports = friends;
